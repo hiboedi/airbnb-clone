1decf6bf9c68caa4a262eb259daa7aee026e7291
hiboedi <hi.boedi8@gmail.com>
2023-02-26 18:42:44 +0700

    edit style

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-4 px-20  md:px-32 py-14 gap-10 bg-slate-100">
-      <div className="space-y-4 text-sm text-gray-500">
+      <div className="space-y-4 text-sm text-gray-800">
         <h5 className="font-bold">ABOUT</h5>
         <p>How Airbnb Works</p>
         <p>Newsrom</p>
@@ -11,7 +11,7 @@ function Footer() {
         <p>Airbnb Plus</p>
         <p>Airbnb Luxe</p>
       </div>
-      <div className="space-y-4 text-sm text-gray-500">
+      <div className="space-y-4 text-sm text-gray-800">
         <h5 className="font-bold">COMMUNITY</h5>
         <p>Accessibility</p>
         <p>This is not real site</p>
@@ -19,7 +19,7 @@ function Footer() {
         <p>Build by Boedi</p>
         <p>On february 26</p>
       </div>
-      <div className="space-y-4 text-sm text-gray-500">
+      <div className="space-y-4 text-sm text-gray-800">
         <h5 className="font-bold">HOST</h5>
         <p>Boedi</p>
         <p>Present</p>
@@ -27,7 +27,7 @@ function Footer() {
         <p>An ordinay guy</p>
         <p>With extraordinary dream</p>
       </div>
-      <div className="space-y-4 text-sm text-gray-500">
+      <div className="space-y-4 text-sm text-gray-800">
         <h5 className="font-bold">SUPPORT</h5>
         <p>@hi.boedi8 on medium patform</p>
         <p>@Boedi420 on twitter</p>
--- pages/index.js
+++ pages/index.js
@@ -64,7 +64,7 @@ export default function Home() {
       </Head>
       <Header />
       <Banner />
-      <main className="max-w-6xl mx-auto px-8 sm:px-16">
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
         <section className="pt-6">
           <h2 className="text-4xl font-semibold pb-5">Explore nearby</h2>
           {/* pull some data on the server - API  */}
