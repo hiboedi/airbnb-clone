f190459b1a5d83211dbc3b26758d712be57073e3
hiboedi <hi.boedi8@gmail.com>
2023-02-27 12:37:33 +0700

    active search feature

--- pages/search.js
+++ pages/search.js
@@ -3,6 +3,94 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { useRouter } from "next/router";
 import { format } from "date-fns";
+import InfoCard from "../components/InfoCard";
+
+const data = [
+  {
+    img: "https://links.papareact.com/xqj",
+    location: "Private room in center of London",
+    title: "Stay at this spacious Edwardian House",
+    description:
+      "1 guest · 1 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Kitchen · Free parking · Washing Machine",
+    star: 4.73,
+    price: "£30 / night",
+    total: "£117 total",
+    long: -0.0022275,
+    lat: 51.5421655,
+  },
+  {
+    img: "https://links.papareact.com/hz2",
+    location: "Private room in center of London",
+    title: "Independant luxury studio apartment",
+    description:
+      "2 guest · 3 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Kitchen",
+    star: 4.3,
+    price: "£40 / night",
+    total: "£157 total",
+    long: -0.095091,
+    lat: 51.48695,
+  },
+  {
+    img: "https://links.papareact.com/uz7",
+    location: "Private room in center of London",
+    title: "London Studio Apartments",
+    description:
+      "4 guest · 4 bedroom · 4 bed · 2 bathrooms · Free parking · Washing Machine",
+    star: 3.8,
+    price: "£35 / night",
+    total: "£207 total",
+    long: -0.135638,
+    lat: 51.521916,
+  },
+  {
+    img: "https://links.papareact.com/6as",
+    location: "Private room in center of London",
+    title: "30 mins to Oxford Street, Excel London",
+    description:
+      "1 guest · 1 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Kitchen · Free parking · Washing Machine",
+    star: 4.1,
+    price: "£55 / night",
+    total: "£320 total",
+    long: -0.069961,
+    lat: 51.472618,
+  },
+  {
+    img: "https://links.papareact.com/xhc",
+    location: "Private room in center of London",
+    title: "Spacious Peaceful Modern Bedroom",
+    description:
+      "3 guest · 1 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Free parking · Dry Cleaning",
+    star: 5.0,
+    price: "£60 / night",
+    total: "£450 total",
+    long: -0.08472,
+    lat: 51.510794,
+  },
+  {
+    img: "https://links.papareact.com/pro",
+    location: "Private room in center of London",
+    title: "The Blue Room In London",
+    description:
+      "2 guest · 1 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Washing Machine",
+    star: 4.23,
+    price: "£65 / night",
+    total: "£480 total",
+    long: -0.094116,
+    lat: 51.51401,
+  },
+  {
+    img: "https://links.papareact.com/8w2",
+    location: "Private room in center of London",
+    title: "5 Star Luxury Apartment",
+    description:
+      "3 guest · 1 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Kitchen · Free parking · Washing Machine",
+    star: 3.85,
+    price: "£90 / night",
+    total: "£650 total",
+    long: -0.109889,
+    lat: 51.521245,
+  },
+];
 
 function Search() {
   const router = useRouter();
@@ -35,6 +123,21 @@ function Search() {
             <p className="button">Rooms and Beds</p>
             <p className="button">More Filters</p>
           </div>
+
+          <div className="flex flex-col">
+            {data.map((item) => (
+              <InfoCard
+                key={item.img}
+                img={item.img}
+                title={item.title}
+                location={item.location}
+                price={item.price}
+                description={item.description}
+                star={item.star}
+                total={item.total}
+              />
+            ))}
+          </div>
         </section>
       </main>
 
@@ -44,3 +147,15 @@ function Search() {
 }
 
 export default Search;
+
+// export async function getServerSideProps() {
+//   const searchResult = await fetch("https://links.papareact.com/isz").then(
+//     (res) => res.json()
+//   );
+
+//   return {
+//     props: {
+//       searchResult: searchResult,
+//     },
+//   };
+// }
