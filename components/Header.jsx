2b7a4ee5434d73222d4b8ad187c5a96fd06c42ae
hiboedi <hi.boedi8@gmail.com>
2023-02-27 07:01:16 +0700

    add search function

--- components/Header.jsx
+++ components/Header.jsx
@@ -10,16 +10,30 @@ import {
 import "react-date-range/dist/styles.css"; // main style file
 import "react-date-range/dist/theme/default.css"; // theme css file
 import { DateRangePicker } from "react-date-range";
+import { useRouter } from "next/router";
 
 function Header() {
   const [searchInput, setSearchInput] = useState("");
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
   const [numberOfGuest, setNumberOfGuest] = useState(1);
+  const router = useRouter();
 
   const handleSelect = (ranges) => {
     setStartDate(ranges.selection.startDate);
     setEndDate(ranges.selection.endDate);
+  };
+
+  const search = () => {
+    router.push({
+      pathname: "/search",
+      query: {
+        location: searchInput,
+        startDate: startDate.toISOString(),
+        endDate: endDate.toISOString(),
+        numberOfGuest: numberOfGuest,
+      },
+    });
   };
 
   const selectionRange = {
@@ -35,7 +49,10 @@ function Header() {
   return (
     <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
       {/* left */}
-      <div className="relative flex items-center h-10 cursor-pointer my-auto">
+      <div
+        onClick={() => router.push("/")}
+        className="relative flex items-center h-10 cursor-pointer my-auto"
+      >
         <Image
           src="https://links.papareact.com/qd3"
           layout="fill"
@@ -44,20 +61,20 @@ function Header() {
         />
       </div>
       {/* middle Search */}
-      <div className="flex items-center border-2 rounded-full py-2 shadow-md">
+      <div className="flex items-center md:border-2 md:shadow-lg rounded-full py-2 ">
         <input
           value={searchInput}
           onChange={(e) => setSearchInput(e.target.value)}
-          className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder:text-gray-400"
+          className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder:text-gray-400 "
           type="text"
-          placeholder="Start your search"
+          placeholder="Search here"
         />
         <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 rounded-full text-white p-2 cursor-pointer mx-2" />
       </div>
       {/* right */}
       <div className="flex items-center justify-end space-x-4 text-gray-500">
         <p className="hidden md:inline cursor-pointer">Become a House</p>
-        <GlobeAltIcon className="h-6 cursor-pointer" />
+        <GlobeAltIcon className="hiiden xs:inline h-6 cursor-pointer" />
         <div className="flex items-center space-x-2 border-2 rounded-full p-2 shadow-md">
           <MenuIcon className="h-6" />
           <UserCircleIcon className="h-6" />
@@ -88,7 +105,9 @@ function Header() {
             <button onClick={resetInput} className="flex-grow text-gray-500">
               Cacncel
             </button>
-            <button className="flex-grow text-red-400">Search</button>
+            <button onClick={search} className="flex-grow text-red-400">
+              Search
+            </button>
           </div>
         </div>
       )}
